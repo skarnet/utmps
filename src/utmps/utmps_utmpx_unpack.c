011971a455d7dcb225d2d2611d3280ca916c098a
Laurent Bercot <ska-skaware@skarnet.org>
2021-04-13 00:14:56 +0000

    Do not null-terminate ut_id

--- src/utmps/utmps_utmpx_unpack.c
+++ src/utmps/utmps_utmpx_unpack.c
@@ -7,7 +7,6 @@ void utmps_utmpx_unpack (char const *s, struct utmpx *b)
 {
   memcpy(b, s, sizeof(struct utmpx)) ;
   b->ut_user[UTMPS_UT_NAMESIZE - 1] = 0 ;
-  b->ut_id[UTMPS_UT_IDSIZE - 1] = 0 ;
   b->ut_line[UTMPS_UT_LINESIZE - 1] = 0 ;
   b->ut_host[UTMPS_UT_HOSTSIZE - 1] = 0 ;
 }
